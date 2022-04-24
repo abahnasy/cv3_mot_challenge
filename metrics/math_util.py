6e926b833f0a8a1bd7f0bb100d67b819ed45b8a9
abahnasy <asbdevai@gmail.com>
2022-04-24 13:18:28 +0200

    remove old files

--- metrics/math_util.py
+++
@@ -1,9 +0,0 @@
-import warnings
-
-import numpy as np
-
-def quiet_divide(a, b):
-    """Quiet divide function that does not warn about (0 / 0)."""
-    with warnings.catch_warnings():
-        warnings.simplefilter('ignore', RuntimeWarning)
-        return np.true_divide(a, b)
